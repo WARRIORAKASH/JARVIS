e53b354cb4c9de0f452e80ed8d32fb8e42ae20d4
Akash <akashwarrior2004@gmail.com>
2024-05-02 08:44:34 +0530

    Translation feature

--- J.A.R.V.I.S.py
+++ J.A.R.V.I.S.py
@@ -15,6 +15,9 @@ import speedtest
 import cv2
 from PIL import Image
 import pytesseract
+import googletrans
+import gtts
+import playsound
 
 for i in range(3):
     a = input("Enter Password to open JARVIS : ")
@@ -76,6 +79,128 @@ def tell_joke():
         speak("Sorry, I couldn't fetch a joke at the moment.")
 
 
+# Language Library
+LANGUAGES = {
+    'afrikaans': 'af',
+    'albanian': 'sq',
+    'amharic': 'am',
+    'arabic': 'ar',
+    'armenian': 'hy',
+    'azerbaijani': 'az',
+    'basque': 'eu',
+    'belarusian': 'be',
+    'bengali': 'bn',
+    'bosnian': 'bs',
+    'bulgarian': 'bg',
+    'catalan': 'ca',
+    'cebuano': 'ceb',
+    'chichewa': 'ny',
+    'chinese (simplified)': 'zh-cn',
+    'chinese (traditional)': 'zh-tw',
+    'corsican': 'co',
+    'croatian': 'hr',
+    'czech': 'cs',
+    'danish': 'da',
+    'dutch': 'nl',
+    'english': 'en',
+    'esperanto': 'eo',
+    'estonian': 'et',
+    'filipino': 'tl',
+    'finnish': 'fi',
+    'french': 'fr',
+    'frisian': 'fy',
+    'galician': 'gl',
+    'georgian': 'ka',
+    'german': 'de',
+    'greek': 'el',
+    'gujarati': 'gu',
+    'haitian creole': 'ht',
+    'hausa': 'ha',
+    'hawaiian': 'haw',
+    'hebrew': 'iw',
+    'hindi': 'hi',
+    'hmong': 'hmn',
+    'hungarian': 'hu',
+    'icelandic': 'is',
+    'igbo': 'ig',
+    'indonesian': 'id',
+    'irish': 'ga',
+    'italian': 'it',
+    'japanese': 'ja',
+    'javanese': 'jw',
+    'kannada': 'kn',
+    'kazakh': 'kk',
+    'khmer': 'km',
+    'korean': 'ko',
+    'kurdish (kurmanji)': 'ku',
+    'kyrgyz': 'ky',
+    'lao': 'lo',
+    'latin': 'la',
+    'latvian': 'lv',
+    'lithuanian': 'lt',
+    'luxembourgish': 'lb',
+    'macedonian': 'mk',
+    'malagasy': 'mg',
+    'malay': 'ms',
+    'malayalam': 'ml',
+    'maltese': 'mt',
+    'maori': 'mi',
+    'marathi': 'mr',
+    'mongolian': 'mn',
+    'myanmar (burmese)': 'my',
+    'nepali': 'ne',
+    'norwegian': 'no',
+    'odia': 'or',
+    'pashto': 'ps',
+    'persian': 'fa',
+    'polish': 'pl',
+    'portuguese': 'pt',
+    'punjabi': 'pa',
+    'romanian': 'ro',
+    'russian': 'ru',
+    'samoan': 'sm',
+    'scots gaelic': 'gd',
+    'serbian': 'sr',
+    'sesotho': 'st',
+    'shona': 'sn',
+    'sindhi': 'sd',
+    'sinhala': 'si',
+    'slovak': 'sk',
+    'slovenian': 'sl',
+    'somali': 'so',
+    'spanish': 'es',
+    'sundanese': 'su',
+    'swahili': 'sw',
+    'swedish': 'sv',
+    'tajik': 'tg',
+    'tamil': 'ta',
+    'telugu': 'te',
+    'thai': 'th',
+    'turkish': 'tr',
+    'ukrainian': 'uk',
+    'urdu': 'ur',
+    'uyghur': 'ug',
+    'uzbek': 'uz',
+    'vietnamese': 'vi',
+    'welsh': 'cy',
+    'xhosa': 'xh',
+    'yiddish': 'yi',
+    'yoruba': 'yo',
+    'zulu': 'zu'
+}
+
+
+def translate_text(text, target_language):
+    translator = googletrans.Translator()
+    target_language_code = LANGUAGES.get(target_language)
+    if target_language_code:
+        translated_text = translator.translate(
+            text, dest=target_language_code).text
+        return translated_text
+    else:
+        return "Language not found in the map"
+
+
 # def sendEmail(to, content):
 #     server = smtplib.SMTP('smtp.gmail.com', 587)
 #     server.ehlo()
@@ -104,6 +229,10 @@ if __name__ == "__main__":
                 elif "quit jarvis" in query:
                     speak("Ok Sir.")
                     exit()
+
+                # JARVIS Introduction
+                elif "introduce yourself" in query:
+                    speak("Hello sir, My name is JARVIS, I am an open source personal voice assistant, I use speech recognition to help users in their daily tasks.")
 
                 # Change Password
                 elif "change password" in query:
@@ -413,4 +542,42 @@ if __name__ == "__main__":
 
                 # Multi Language Translator
                 elif "translate" in query:
-                    pass
+                    if "text" in query:
+                        text_to_translate = input("Enter the Text : ")
+                        target_language = input("Enter Language : ")
+                        translated_text = translate_text(
+                            text_to_translate, target_language)
+                        converted_audio = gtts.gTTS(
+                            translated_text, lang=LANGUAGES.get(target_language))
+                        converted_audio.save("translate.mp3")
+                        playsound.playsound("translate.mp3")
+                        print(translated_text)
+
+                    elif "image" in query:
+                        camera = cv2.VideoCapture(0)
+                        while True:
+                            _, image = camera.read()
+                            cv2.imshow('Text detection', image)
+                            if cv2.waitKey(1) & 0xFF == ord('s'):
+                                cv2.imwrite('image.jpg', image)
+                                break
+                        camera.release()
+                        cv2.destroyAllWindows()
+
+                        def tesseract():
+                            path_to_tesseract = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+                            Imagepath = "image.jpg"
+                            pytesseract.pytesseract.tesseract_cmd = path_to_tesseract
+                            text = pytesseract.image_to_string(
+                                Image.open(Imagepath))
+                            text_to_translate = text[:-1]
+                            return text_to_translate
+                        text_to_translate = tesseract()
+                        target_language = input("Enter Language : ")
+                        translated_text = translate_text(
+                            text_to_translate, target_language)
+                        converted_audio = gtts.gTTS(
+                            translated_text, lang=LANGUAGES.get(target_language))
+                        converted_audio.save("translate.mp3")
+                        playsound.playsound("translate.mp3")
+                        print(translated_text)
